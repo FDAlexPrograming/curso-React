7dc3a8a8c129adf18d7f7850a676865838a5f7c7
Alex Farias dos Santos <fariasdossantosalex@gmail.com>
2023-02-27 10:06:13 -0300

    login y register terminados

---
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { AuthRoutes } from "../auth/pages/routes/AuthRoutes"
+import { JournalRoutes } from "../journal/routes/JournalRoutes"
+
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+
+        {/* Login y Registro */}
+
+        <Route path="auth/*" element={ <AuthRoutes /> } />
+
+        {/* Journal App */}
+
+        <Route path="/*" element={ <JournalRoutes /> } />
+
+
+    </Routes>
+  )
+}
