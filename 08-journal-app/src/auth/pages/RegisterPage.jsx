db4e7871b1d1d580cc03884315240246ede054dd
Alex Farias dos Santos <fariasdossantosalex@gmail.com>
2023-05-15 11:29:24 -0300

    register funcionalidad

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,47 +1,93 @@
 
-import { Google } from "@mui/icons-material"
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from "react-router-dom"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout"
+import { useForm } from "../../hooks"
 
+const formData = {
+  email: 'fariasdossantosalex@gmail.com',
+  password: '123456',
+  displayName: 'Alex Farias',
+}
 export const RegisterPage = () => {
+
+
+  const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log(formState)
+  }
+
+
   return (
     <AuthLayout title="Crear cuenta">
-      <form >
-          <Grid container >
+      <form onSubmit= { onSubmit } >
+        <Grid container >
 
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField label="Nombre Completo" type="text" placeholder="Nombre Completo" fullWidth/>
-            </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField 
+            label="Nombre Completo" 
+            type="text" 
+            placeholder="Nombre Completo" 
+            fullWidth
+            name="displayName"
+            value={ displayName }
+            onChange={ onInputChange }
+            />
+          </Grid>
 
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField label="Correo" type="email" placeholder="correo@google.com" fullWidth/>
-            </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField 
+            label="Correo" 
+            type="email" 
+            placeholder="correo@google.com" 
+            fullWidth
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+            />
+          </Grid>
 
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField label="Contraseña" type="password" placeholder="Contraseña" fullWidth autoComplete="contraseña actual"/>
-            </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField 
+            label="Contraseña" 
+            type="password" 
+            placeholder="Contraseña" 
+            fullWidth 
+            autoComplete="contraseña actual"
+            name="password"
+            value={ password }
+            onChange={ onInputChange }
+            
+            />
+          </Grid>
 
-            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 2 }}>
-              <Grid item xs={ 12 }>
-                <Button variant="contained" fullWidth>
-                  Crear Cuenta
-                </Button>
-              </Grid>
-            </Grid>
-
-            <Grid container direction='row' justifyContent='end'>
-              <Typography sx={{ m: 1 }}> ¿Ya tienes cuenta? </Typography>
-                <Button component={ RouterLink } to="/auth/login" sx={{ 
-                                                                    fontSize: '15px',
-                                                                  ':hover':{ backgroundColor: '#6c6b73', color:'white', transition:'.5s', },
-                                                                    }}  
+          <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
+            <Grid item xs={12}>
+              <Button 
+                variant="contained" 
+                fullWidth
+                type="submit" 
                 >
-                  Ingresar
-                </Button>
+                Crear Cuenta
+              </Button>
             </Grid>
           </Grid>
-        </form>
-    </AuthLayout>        
+
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ m: 1 }}> ¿Ya tienes cuenta? </Typography>
+            <Button component={RouterLink} to="/auth/login" sx={{
+              fontSize: '15px',
+              ':hover': { backgroundColor: '#6c6b73', color: 'white', transition: '.5s', },
+            }}
+            >
+              Ingresar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
